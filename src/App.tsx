8099fad2fa54a4e63de4398716f9000c9d6b349c
Mrinal Paul <paulmrinal12@gmail.com>
2026-09-26 14:06:52 +0530

    Fix TypeScript unused React import error

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { Navbar } from './components/layout/Navbar';
 import { Preloader } from './components/layout/Preloader';
 import { Hero } from './components/sections/Hero';
